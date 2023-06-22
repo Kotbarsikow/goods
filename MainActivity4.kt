dd59ac72703b822c927fdbee41b18ebb501b55b7
Kotbarsikow <98335967+Kotbarsikow@users.noreply.github.com>
2023-06-22 16:59:59 +0300

    Update MainActivity4.kt

--- MainActivity4.kt
+++ MainActivity4.kt
@@ -1,5 +1,5 @@
 package com.example.stanapp
-
+//код сторінки для детального показу товарів
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
